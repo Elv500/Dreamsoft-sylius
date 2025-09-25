6e42ec68aeb65c3d377fa595edb7036eb41efa6d
Elv500 <elvis.alvarez.2001@gmail.com>
2025-09-24 22:23:38 -0400

    Automatizando pruebas de seguridad: Sin token y token invalido, y code inexistente

--- src/assertions/taxons/error_assertion.py
+++ src/assertions/taxons/error_assertion.py
@@ -11,3 +11,13 @@ class AssertionTaxonsError:
             assert response_json["message"] == message, "Mensaje de error no coincide"
         except AssertionError as e:
             pytest.fail(f"[TaxonsError Errors] {e}")
+
+    @staticmethod
+    def assert_taxons_error_request(response_json, status, detail):
+        try:
+            assert "status" in response_json, "'status' no está en la respuesta"
+            assert "detail" in response_json, "'detail' no está en la respuesta"
+            assert response_json["status"] == status, "Error status no coincide"
+            assert response_json["detail"] == detail, "Error detail no coincide"
+        except AssertionError as e:
+            pytest.fail(f"[TaxonsError Errors Request] {e}")
